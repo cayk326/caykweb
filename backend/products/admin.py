bb7d1b35da77907de875c03840c67fbe1768c510
cayk326 <yuta0326mj@gmail.com>
2023-05-22 01:02:21 +0900

    add new model

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Book, Grocery, Hello
+from .models import Category, Book, Grocery, Hello, ReadBook
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Category)
 admin.site.register(Book)
 admin.site.register(Grocery)
 admin.site.register(Hello)
+admin.site.register(ReadBook)
--- backend/products/models.py
+++ backend/products/models.py
@@ -51,3 +51,15 @@ class Hello(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+    
+class ReadBook(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=100)
+    comment = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
